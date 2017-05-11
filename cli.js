b69bd146ab4b6ee15614f41c06ed09d16874ad00
Darío Javier Cravero <dario@uxtemple.com>
2017-05-11 20:57:20 +0100

    feat: watch

--- cli.js
+++ cli.js
@@ -1,62 +1,80 @@
-const { readFileSync, writeFileSync } = require('fs')
+const { readFileSync, statSync, writeFileSync } = require('fs')
 const { morph, pathToName } = require('./lib.js')
+const chalk = require('chalk')
+const startWatching = require('./watch')
 
-let {
+const {
+  _,
   as,
   compile,
   help,
-  input,
-  output,
   pretty,
+  watch,
 } = require('minimist')(process.argv.slice(2), {
   alias: {
     help: 'h',
-    input: 'i',
-    o: 'output',
   },
-  booleans: ['help'],
+  booleans: ['help', 'watch'],
 
   default: {
     as: 'react-dom',
     compile: false,
     pretty: false,
+    watch: false,
   },
 })
 
 if (help) {
-  console.log(
-    `
-  views-morph
-    --as            target platform             defaults to react-dom
-    --compile       if true, produces raw js    defaults to false
-    --input -i      /path/to/some.view
-    --name          the name of the view        defaults to the input file name without
-                                                the extension in pascal case format. Eg: for input
-                                                file-is-view.view you the name is FileIsView.
-    --output -o     /path/to/output.js          defaults to stdout
-    --pretty        nicely format output code   defaults to false
-  `
-  )
+  console.log(`
+  views-morph [file or directory]
+    --as            target platform
+                      react-dom (default)
+                      react-native
+
+    --compile       if true, produces ES5 JS, defaults to false
+    --pretty        format output code, defaults to false
+    --watch         watch a directory and produce .view.js files
+  `)
 
   process.exit()
 }
+
+const input = Array.isArray(_) && _[0]
 
 if (!input) {
   console.error(
-    'You need to specify an input file. Eg run views-morph --input some.view'
+    'You need to specify an input file. Eg run views-morph some.view'
   )
   process.exit()
 }
 
-const code = morph(readFileSync(input, 'utf-8'), {
-  as,
-  compile,
-  name: pathToName(input),
-  pretty,
-})
+if (watch) {
+  if (!statSync(input).isDirectory()) {
+    console.error(`You need to specify an input directory to watch. ${input} is a file.`)
+    process.exit()
+  }
 
-if (output) {
-  writeFileSync(output, code)
+  console.log(`Will morph files at '${chalk.green(input)}' as ${chalk.green(as)}\n`)
+  console.log(chalk.yellow('A'), ' = Added')
+  console.log(chalk.blue('D'), ` = View deleted`)
+  console.log(chalk.green('M'), ` = Morphed`)
+  console.log(chalk.red('M'), ` = Morph failed`)
+  console.log(chalk.magenta('!'), ` = View doesn't exist but is being used`)
+  console.log('\n\nPress', chalk.blue('ctrl+c'), 'to stop at any time.\n\n')
+
+  startWatching({
+    as,
+    compile,
+    pretty,
+    src: input,
+  })
 } else {
+  const code = morph(readFileSync(input, 'utf-8'), {
+    as,
+    compile,
+    name: pathToName(input),
+    pretty,
+  })
+
   console.log(code)
 }
---
+++ watch.js
@@ -0,0 +1,130 @@
+const { getViewNotFound, morph, pathToName, types } = require('./lib.js')
+const { extname, relative } = require('path')
+const { readFile, readFileSync, writeFile } = require('fs')
+const chalk = require('chalk')
+const globule = require('globule')
+const toPascalCase = require('to-pascal-case')
+const watch = require('gaze')
+
+const { ACTION, TELEPORT } = types
+
+const isJs = f => extname(f) === '.js'
+const isMorphedView = f => /\.view\.js$/.test(f)
+const isView = f => extname(f) === '.view'
+
+module.exports = options => {
+  let { as, compile, map, shared, src, pretty, viewNotFound } = Object.assign(
+    {
+      as: 'react-dom',
+      compile: true,
+      map: {},
+      pretty: false,
+      src: process.cwd(),
+    },
+    options
+  )
+
+  if (!shared) shared = `views-blocks-${as}`
+  if (!viewNotFound)
+    viewNotFound = name => {
+      const warning = `${src}/${name}.view doesn't exist but it is being used. Create the file!`
+      console.log(chalk.magenta(`! ${warning}`))
+      return getViewNotFound(as, name, warning)
+    }
+
+  const jsComponents = {}
+  const isJsComponent = f => {
+    if (jsComponents.hasOwnProperty(f)) return jsComponents[f]
+    return (jsComponents[f] = /import React/.test(readFileSync(f, 'utf-8')))
+  }
+
+  const filter = fn => f => {
+    if (isMorphedView(f) || (isJs(f) && !isJsComponent(f))) return
+
+    fn(f)
+  }
+
+  const getImport = name => {
+    switch (name) {
+      case ACTION:
+      case TELEPORT:
+        return `import { ${name} } from '${shared}'`
+      default:
+        return views[name]
+          ? `import ${name} from '${views[name]}'`
+          : viewNotFound(name)
+    }
+  }
+
+  const views = map
+
+  const addView = filter(f => {
+    const { file, view } = toViewPath(f)
+    console.log(chalk.yellow('A'), view, chalk.dim(`-> ${f}`))
+    views[view] = file
+
+    if (isView(file)) morphView(f)
+  })
+
+  const morphView = filter(f => {
+    const { file, view } = toViewPath(f)
+
+    readFile(f, 'utf-8', (err, source) => {
+      if (err) {
+        return console.error(chalk.red('M'), view, err)
+      }
+
+      const code = morph(source, {
+        as,
+        compile,
+        name: view,
+        getImport,
+        pretty,
+      })
+
+      writeFile(`${f}.js`, code, err => {
+        if (err) {
+          return console.error(chalk.red('M'), view, err)
+        }
+
+        console.log(chalk.green('M'), view)
+      })
+    })
+  })
+
+  const toViewPath = f => {
+    const file = relative(src, f)
+    const view = toPascalCase(file.replace(/\.(view|js)/g, ''))
+
+    return {
+      file: `./${file}`,
+      view,
+    }
+  }
+
+  const watcherOptions = {
+    filter: f => !/node_modules/.test(f) && !isMorphedView(f),
+  }
+  const watcherPattern = ['**/*.js', '**/*.view']
+  globule.find(watcherPattern, watcherOptions).forEach(addView)
+
+  watch(watcherPattern, watcherOptions, (err, watcher) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    // TODO see how we can force a rebuild when a file gets added/deleted
+    watcher.on('added', addView)
+    watcher.on('changed', morphView)
+    watcher.on(
+      'deleted',
+      filter(f => {
+        const { view } = toViewPath(f)
+        console.log(chalk.blue('D'), view)
+
+        delete views[view]
+      })
+    )
+  })
+}
