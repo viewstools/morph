476d053e4d9131bc669b263031ac157cc07608f8
Darío Javier Cravero <dario@uxtemple.com>
2017-06-15 21:40:27 +0100

    fix: don't pass isActive to button

--- morph/react-dom/is-valid-property-for-block.js
+++ morph/react-dom/is-valid-property-for-block.js
@@ -1,2 +1,2 @@
-const blacklist = ['backgroundSize', 'teleportTo', 'goTo']
+const blacklist = ['backgroundSize', 'teleportTo', 'goTo', 'isActive']
 export default (node, parent) => !blacklist.includes(node.key.value)
