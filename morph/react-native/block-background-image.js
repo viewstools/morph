999b96125c9cd5fa12fe71cdcd212cca10b04eaf
Darío Javier Cravero <dario@uxtemple.com>
2017-09-12 22:07:10 +0100

    fix: workaround border radius on background image

--- morph/react-native/block-background-image.js
+++ morph/react-native/block-background-image.js
@@ -1,9 +1,26 @@
-import { getObjectAsString } from '../utils.js'
+import { hasProp, getObjectAsString, getProp } from '../utils.js'
 import wrap from '../react/wrap.js'
+
+const BORDER_RADIUS = [
+  'borderRadius',
+  'borderTopLeftRadius',
+  'borderTopRightRadius',
+  'borderBottomLeftRadius',
+  'borderBottomRightRadius',
+]
 
 export const enter = (node, parent, state) => {
   if (node.backgroundImage) {
     const source = wrap(getObjectAsString({ uri: node.backgroundImage }))
     state.render.push(` resizeMode="cover" source=${source}`)
+
+    // hack until https://github.com/facebook/react-native/issues/8885
+    // is fixed
+    BORDER_RADIUS.forEach(prop => {
+      if (hasProp(node, prop)) {
+        const propNode = getProp(node, prop)
+        state.render.push(` ${prop}={${propNode.value.value}}`)
+      }
+    })
   }
 }
