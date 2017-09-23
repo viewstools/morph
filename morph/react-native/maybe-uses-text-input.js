3440ab96652cd1484810aa9cabf56e820c248a33
Darío Javier Cravero <dario@uxtemple.com>
2017-09-23 17:18:03 +0100

    fix: import TouchableWithoutFeedback when TextInput is used

--- morph/react-native/maybe-uses-text-input.js
+++ morph/react-native/maybe-uses-text-input.js
@@ -1,7 +1,8 @@
 export default state => {
   if (state.uses.includes('TextInput')) {
-    state.uses.push('KeyboardAvoidingView')
-    state.uses.push('DismissKeyboard')
+    state.use('DismissKeyboard')
+    state.use('KeyboardAvoidingView')
+    state.use('TouchableWithoutFeedback')
 
     state.render = [
       `<KeyboardAvoidingView behavior='position'>`,
