2cc9cf951f9ef8aa7e7f947b2470a286bb468b2d
Darío Javier Cravero <dario@uxtemple.com>
2017-09-26 18:36:58 +0100

    fix: disable automatic KeyboardAvoidingView for now

--- morph/react-native/maybe-uses-text-input.js
+++ morph/react-native/maybe-uses-text-input.js
@@ -1,15 +1,15 @@
+// TODO revisit with https://github.com/viewsdx/keyboard-avoiding-and-dismissing-view
 export default state => {
-  if (state.uses.includes('TextInput')) {
-    state.use('DismissKeyboard')
-    state.use('KeyboardAvoidingView')
-    state.use('TouchableWithoutFeedback')
-
-    state.render = [
-      `<KeyboardAvoidingView behavior='position'>`,
-      `<TouchableWithoutFeedback onPress={dismissKeyboard}>`,
-      ...state.render,
-      `</TouchableWithoutFeedback>`,
-      `</KeyboardAvoidingView>`,
-    ]
-  }
+  // if (state.uses.includes('TextInput')) {
+  //   state.use('DismissKeyboard')
+  //   state.use('KeyboardAvoidingView')
+  //   state.use('TouchableWithoutFeedback')
+  //   state.render = [
+  //     `<KeyboardAvoidingView behavior='position'>`,
+  //     `<TouchableWithoutFeedback onPress={dismissKeyboard}>`,
+  //     ...state.render,
+  //     `</TouchableWithoutFeedback>`,
+  //     `</KeyboardAvoidingView>`,
+  //   ]
+  // }
 }
