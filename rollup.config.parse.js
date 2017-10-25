c00a59f6098deeed61f2530935a1d40043489e70
Darío Javier Cravero <dario@uxtemple.com>
2017-10-25 13:03:37 +0100

    fix: dependencies and build

--- rollup.config.parse.js
+++ rollup.config.parse.js
@@ -8,6 +8,24 @@ export default {
   plugins: [
     babel({
       plugins: ['external-helpers', 'transform-object-rest-spread'],
+      presets: [
+        [
+          'env',
+          {
+            targets: {
+              // React parses on ie 9, so we should too
+              ie: 11,
+              // We currently minify with uglify
+              // Remove after https://github.com/mishoo/UglifyJS2/issues/448
+              uglify: true,
+            },
+            // Disable polyfill transforms
+            useBuiltIns: false,
+            // Do not transform modules to CJS
+            modules: false,
+          },
+        ],
+      ],
     }),
   ],
 }
