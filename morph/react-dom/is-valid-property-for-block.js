48ce213948782cda76d89dc9726a7711a623fb32
Darío Javier Cravero <dario@uxtemple.com>
2017-12-03 10:11:29 +0000

    refactor: logic on dom check for valid property

--- morph/react-dom/is-valid-property-for-block.js
+++ morph/react-dom/is-valid-property-for-block.js
@@ -4,13 +4,8 @@ const blacklistDebug = ['autofocus', 'tabindex']
 export default (node, parent, state) => {
   const key = node.key.value.toLowerCase()
 
-  if (parent.parent.isCapture) {
-    console.log(key, state.debug && blacklistDebug.includes(key))
-  }
-
-  if (blacklist.includes(key)) return false
-
-  if (state.debug && blacklistDebug.includes(key)) return false
-
-  return true
+  return !(
+    blacklist.includes(key) ||
+    (state.debug && blacklistDebug.includes(key))
+  )
 }
