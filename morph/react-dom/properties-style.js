c304b8a6389e1b45f3e5b549496d21d538668fb7
amymc <amy.mccarthy@hmhco.com>
2017-12-07 11:25:28 +0000

    feat: render pseudo classes

--- morph/react-dom/properties-style.js
+++ morph/react-dom/properties-style.js
@@ -7,35 +7,19 @@ let dynamicCss = ''
 const asDynamicCss = (style, styleKey) => {
   const props = Object.keys(style)
 
-  debugger
   if (styleKey !== 'base') dynamicCss += `&:${styleKey} {`
 
   props.forEach(prop => {
-    dynamicCss += `${toSlugCase(prop)}: \${props =>\ ${style[`${prop}`]}\}`
+    dynamicCss += `${toSlugCase(prop)}: \${props =>\ ${style[`${prop}`]}\};`
   })
 
   if (styleKey !== 'base') dynamicCss += `}`
 
-  debugger
   return dynamicCss
 }
 
-const getStyledComponent = (name, base, style, styleKey, isLastItem) => {
-  if (!isLastItem) {
-    asDynamicCss(style, styleKey)
-  } else {
-    console.log(
-      'wtffff',
-      `const ${name} = styled('${base}')\`${asDynamicCss(style, styleKey)}\``
-    )
-    debugger
-
-    return `const ${name} = styled('${base}')\`${asDynamicCss(
-      style,
-      styleKey
-    )}\``
-  }
-}
+const getStyledComponent = (name, base, style, styleKey) =>
+  `const ${name} = styled('${base}')\`${asDynamicCss(style, styleKey)}\``
 
 export const leave = (node, parent, state) => {
   if (hasKeysInChildren(node.style.static)) {
@@ -60,36 +44,27 @@ export const leave = (node, parent, state) => {
     const filteredDynamicStyles = Object.keys(
       node.style.dynamic
     ).filter((dynamicKey, index) => {
-      console.log(
-        'dynamicProp',
-        hasKeysInChildren(node.style.dynamic[dynamicKey])
-      )
       return hasKeysInChildren(node.style.dynamic[dynamicKey])
-      // code += getStyledComponent(block.is, block.name.finalValue, node.style.dynamic[dynamicKey], dynamicKey);
     })
-    console.log('filteredDynamicStyles', filteredDynamicStyles)
-    debugger
 
     const filteredLength = filteredDynamicStyles.length
-    code += filteredDynamicStyles.forEach((styleKey, index) => {
-      return getStyledComponent(
-        block.is,
-        block.name.finalValue,
-        node.style.dynamic[styleKey],
-        styleKey,
-        filteredLength - 1 === index
-      )
+    filteredDynamicStyles.forEach((styleKey, index) => {
+      if (filteredLength - 1 !== index) {
+        asDynamicCss(node.style.dynamic[styleKey], styleKey)
+      } else {
+        code = getStyledComponent(
+          block.is,
+          block.name.finalValue,
+          node.style.dynamic[styleKey],
+          styleKey
+        )
+      }
     })
-    // const code = getStyledComponent(node.dynamicStyleComponent.name, node.dynamicStyleComponent.tag, node.style.dynamic);
-    // const code = getStyledComponent(block.is, block.name.finalValue, node.style.dynamic.base);
 
     console.log(state.render)
-    // TODO replace the tag for the one we need to
+
     state.render[0] = `<${block.is}`
     state.stylesDynamic.push(code)
     block.name.finalValue = block.is
-
-    // const dynamic = getObjectAsString(node.style.dynamic.base);
-    // state.render.push(` style={${dynamic}}`);
   }
 }
