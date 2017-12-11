8a511207318b965a0ad37a0335aa210dd5ecbd14
Darío Javier Cravero <dario@uxtemple.com>
2017-12-11 18:49:23 +0000

    feat: support images passed through props

--- morph/react-native/get-value-for-property.js
+++ morph/react-native/get-value-for-property.js
@@ -1,4 +1,4 @@
-import { isCode, getScopedProps } from '../utils.js'
+import { getScopedProps } from '../utils.js'
 import safe from '../react/safe.js'
 import toCamelCase from 'to-camel-case'
 
@@ -7,9 +7,12 @@ const isUrl = str => /^https?:\/\//.test(str)
 const getImageSource = (node, state) => {
   const { value } = node.value
 
-  if (node.tags.code || isUrl(value)) {
-    const uri = isCode(value) ? value : `"${value}"`
-    return `{{ uri: ${uri} }}`
+  if (isUrl(value)) {
+    return `{{ uri: "${value}" }}`
+  } else if (node.tags.code) {
+    return `{/^https?:\\/\\//.test(${value})? { uri: ${value} } : ${state.debug
+      ? 'require'
+      : 'requireImage'}(${value})}`
   } else {
     const name = toCamelCase(value)
     if (!state.images.includes(value)) {
@@ -28,17 +31,14 @@ export default (node, parent, state) => {
 
   switch (node.value.type) {
     case 'Literal':
-      // TODO support sccoped source
-      // TODO support SVGs
-      // TODO support local files passed as props
-      // I guess we either do this dynamically or we determine the value being
-      // passed based off the link between views
+      // TODO support scoped source
+      // TODO SVGs will need a different treatment when the source is a prop
       if (key === 'source' && parent.parent.name.value === 'Image') {
-        return parent.parent.isSvg
-          ? {}
-          : {
-              source: getImageSource(node, state),
-            }
+        return (
+          !parent.parent.isSvg && {
+            source: getImageSource(node, state),
+          }
+        )
       } else if (parent.parent.scoped.hasOwnProperty(key)) {
         return {
           [key]: safe(getScopedProps(node, parent.parent)),
