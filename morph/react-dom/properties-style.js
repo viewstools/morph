e4759ed7698f6cda5f4d8d0dfe1d92d7abf60c79
amymc <amy.mccarthy@hmhco.com>
2017-12-11 17:04:38 +0000

    wip: apply child hover styles on parent hover

--- morph/react-dom/properties-style.js
+++ morph/react-dom/properties-style.js
@@ -4,10 +4,16 @@ import toSlugCase from 'to-slug-case'
 
 let dynamicCss
 
-const asDynamicCss = (style, styleKey) => {
+const asDynamicCss = (style, styleKey, parentEl) => {
   const props = Object.keys(style)
 
-  if (styleKey !== 'base') dynamicCss += `&:${styleKey} {`
+  debugger
+  if (styleKey !== 'base' && parentEl) {
+    dynamicCss += `\${${parentEl}}\:${styleKey} & {`
+  } else if (styleKey !== 'base') {
+    dynamicCss += `&:${styleKey} {`
+  }
+  // ${ButtonOne}:hover & {
 
   props.forEach(prop => {
     dynamicCss += `${toSlugCase(prop)}: \${props =>\ ${style[`${prop}`]}\};`
@@ -18,8 +24,30 @@ const asDynamicCss = (style, styleKey) => {
   return dynamicCss
 }
 
-const getStyledComponent = (name, base, style, styleKey) =>
-  `const ${name} = styled('${base}')\`${asDynamicCss(style, styleKey)}\``
+const getStyledComponent = (name, base, style, styleKey, parentEl) =>
+  `const ${name} = styled('${base}')\`${asDynamicCss(
+    style,
+    styleKey,
+    parentEl
+  )}\``
+
+const checkParentStem = (node, styleKey) => {
+  debugger
+  if (styleKey === 'base' || !node.parent.parent) return
+
+  //const nodeStyleStem = getStyleType(node);
+
+  //if (nodeStyleStem !== 'base' && node.parent.parent) {
+  const matchedParentEl = node.parent.parent.parent
+  const matchingParentStem = matchedParentEl.properties.list.find(
+    prop => prop.key.valueRaw.toLowerCase().indexOf(styleKey) > -1
+  )
+  //}
+
+  if (matchingParentStem) {
+    return matchedParentEl.is || matchedParentEl.name.value
+  }
+}
 
 export const leave = (node, parent, state) => {
   if (hasKeysInChildren(node.style.static)) {
@@ -49,16 +77,21 @@ export const leave = (node, parent, state) => {
       return hasKeysInChildren(node.style.dynamic[dynamicKey])
     })
 
+    debugger
+
     const filteredLength = filteredDynamicStyles.length
     filteredDynamicStyles.forEach((styleKey, index) => {
+      debugger
+      const parentEl = checkParentStem(node, styleKey)
       if (filteredLength - 1 !== index) {
-        asDynamicCss(node.style.dynamic[styleKey], styleKey)
+        asDynamicCss(node.style.dynamic[styleKey], styleKey, parentEl)
       } else {
         code = getStyledComponent(
           blockName,
           block.name.finalValue,
           node.style.dynamic[styleKey],
-          styleKey
+          styleKey,
+          parentEl
         )
       }
     })
