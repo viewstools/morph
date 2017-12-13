fe85ab42cd6992961ed436776314d588dc64d6a3
amymc <amy.mccarthy@hmhco.com>
2017-12-13 15:36:43 +0000

    chore: remove maybe-uses-stylesheet

--- morph/react-dom/maybe-uses-style-sheet.js
+++
@@ -1,9 +0,0 @@
-import { hasKeys } from '../utils.js'
-
-// TODO don't import glam if styles aren't inlined and there are no dynamic
-// styles
-export default state => {
-  if (hasKeys(state.styles) && state.inlineStyles) {
-    state.uses.push('glam')
-  }
-}
