484bad0a8d57087eff05720168b96b646d093a12
amymc <amy.mccarthy2@mail.dcu.ie>
2017-12-16 14:20:15 +0000

    fix: ternary conditional

--- morph/react-dom/properties-style.js
+++ morph/react-dom/properties-style.js
@@ -61,14 +61,15 @@ export const leave = (node, parent, state) => {
 
   if (hasKeysInChildren(node.style.dynamic)) {
     const block = node.parent
-    const blockName = block.is || block.name.value
+    let blockName = block.is || block.name.value
 
     const alreadyExists = state.render.filter(item =>
       item.match(`<${blockName}`)
     )
-    blockName = alreadyExists
-      ? `${blockName}${alreadyExists.length + 1}`
-      : blockName
+    blockName =
+      alreadyExists.length > 0
+        ? `${blockName}${alreadyExists.length + 1}`
+        : blockName
 
     let code = ''
     dynamicCss = ''
