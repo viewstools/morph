fe98cec645bdab52544262db6f2738d6796c9192
amymc <amy.mccarthy@hmhco.com>
2018-05-14 16:17:31 +0100

    fix: use single quotes in local container

--- ensure-local-container.js
+++ ensure-local-container.js
@@ -36,7 +36,7 @@ export default class LocalContainer extends Container {
   constructor() {
     super()
 
-    const supported = ${JSON.stringify(supported)}
+    const supported = ${JSON.stringify(supported).replace(/["]/g, "'")}
 
     this.state = {
       lang: getInitialLanguage(supported),
@@ -57,7 +57,7 @@ export default class LocalContainer extends Container {
   constructor() {
     super()
 
-    const supported = ${JSON.stringify(supported)}
+    const supported = ${JSON.stringify(supported).replace(/["]/g, "'")}
 
     this.state = {
       lang: supported[0],
