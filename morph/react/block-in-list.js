ec13b3d493aa167a1d91dda46807ccd0afe70f85
Darío Javier Cravero <dario@uxtemple.com>
2018-05-30 00:16:46 +0100

    fix: only render key if it isnt a flatlist

--- morph/react/block-in-list.js
+++ morph/react/block-in-list.js
@@ -2,6 +2,10 @@ import { isList } from '../utils.js'
 
 export function enter(node, parent, state) {
   if (isList(parent)) {
-    state.render.push(' index={index} {...item} key={item.id || index}')
+    state.render.push(' index={index} {...item}')
+
+    if (parent.nameFinal !== 'FlatList') {
+      state.render.push(` key={item.id || index}`)
+    }
   }
 }
