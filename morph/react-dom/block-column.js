8f9aa0009c9e311d4e4bb494fc319ad2a82ca34d
amymc <amy.mccarthy@hmhco.com>
2018-06-30 18:58:37 +0100

    fix: remove width from column css

--- morph/react-dom/block-column.js
+++ morph/react-dom/block-column.js
@@ -2,12 +2,17 @@ import { getLabel, getProp, isColumn } from '../utils.js'
 
 export function enter(node, parent, state) {
   if (isColumn(node)) {
-    const dataKey = getProp(node, 'key').value
-    const width = getProp(node, 'width').value
+    debugger
+    const dataKey = getProp(node, 'key')
+    const width = getProp(node, 'width')
     const label = getLabel(node)
 
+    if (width) {
+      node.properties.splice(node.properties.indexOf(width), 1)
+    }
+
     state.render.push(
-      ` dataKey="${dataKey}" width={${width}} ${
+      ` dataKey="${dataKey.value}" width={${width.value}} ${
         label ? `label="${label}"` : ''
       }`
     )
