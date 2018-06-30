0cfd9a145991497c279daeffa635e7599d4a973e
Darío Javier Cravero <dario@uxtemple.com>
2018-06-30 14:04:52 +0100

    fix: show numpad in iOS Safari for CaptureNumber

--- morph/react-dom/block-capture.js
+++ morph/react-dom/block-capture.js
@@ -23,6 +23,12 @@ export const enter = (node, parent, state) => {
     state.render.push(` type='${typesMap[blockType]}'`)
   }
 
+  // fix for iOS Safari to show the numpad on
+  // http://danielfriesen.name/blog/2013/09/19/input-type-number-and-ios-numeric-keypad/
+  if (blockType === 'CaptureNumber') {
+    state.render.push(` inputmode="numeric" pattern="[0-9]*"`)
+  }
+
   // if you specify a value, then you're supposed to manage the input from
   // outside the view
   if (getProp(node, 'value')) return
