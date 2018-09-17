ec8cf8117d87f093cf4a520ab33c06fb4e14b688
amy mccarthy <amy.mccarthy2@mail.dcu.ie>
2018-09-17 22:03:21 +0100

    Ensure props are converted to hyphenated (#144)
    * fix: convert props to  hyphenated when parsing
    
    * fix: remove fontFamily
    
    * chore: add snapshot
    
    * chore: add test case with view

--- parse/helpers.js
+++ parse/helpers.js
@@ -2,6 +2,7 @@ import { isRowStyle, isStyle, STYLE } from './prop-is-style.js'
 import DidYouMeanMatcher from './did-you-mean.js'
 import isNumber from './prop-is-number.js'
 import locales from 'i18n-locales'
+import toSlugCase from 'to-slug-case'
 
 const LOCAL_SCOPES = locales.map(item => item.replace(/-/g, ''))
 
@@ -308,7 +309,7 @@ export const getUnsupportedShorthandExpanded = (name, value) => {
 
   return []
 }
-export const getValue = value => {
+export const getValue = (value, name) => {
   if (isFloat(value)) {
     return parseFloat(value, 10)
   } else if (isInt(value)) {
@@ -318,7 +319,7 @@ export const getValue = value => {
   } else if (isBool(value)) {
     return isTrue(value)
   } else {
-    return value
+    return maybeMakeHyphenated(value, name)
   }
 }
 
@@ -354,3 +355,35 @@ export const isTextInterpolation = (block, previous) => {
     previousText.value.includes(block.is || block.name)
   )
 }
+
+const MAYBE_HYPHENATED_STYLE_PROPS = [
+  'alignContent',
+  'alignItems',
+  'alignSelf',
+  'backgroundBlendMode',
+  'backgroundClip',
+  'backgroudOrigin',
+  'backgroundRepeat',
+  'boxSizing',
+  'clear',
+  'cursor',
+  'flexBasis',
+  'flexDirection',
+  'flexFlow',
+  'flexWrap',
+  'float',
+  'fontStretch',
+  'justifyContent',
+  'objectFit',
+  'overflowWrap',
+  'textAlign',
+  'textDecorationLine',
+  'textTransform',
+  'whiteSpace',
+  'wordBreak',
+]
+
+export const maybeMakeHyphenated = (value, name) =>
+  MAYBE_HYPHENATED_STYLE_PROPS.includes(name) && /^[a-zA-Z]+$/.test(value)
+    ? toSlugCase(value)
+    : value
