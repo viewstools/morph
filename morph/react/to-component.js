c6f96120c279d0141d3f3f6f872d20e5d618d2b7
Darío Javier Cravero <dario@uxtemple.com>
2018-11-05 10:14:29 +0100

    fix: dismiss duplicated keys warning in morphed files until we fix #119

--- morph/react/to-component.js
+++ morph/react/to-component.js
@@ -8,7 +8,7 @@ import getLocals from './get-locals.js'
 export default ({ getImport, getStyles, name, state }) => {
   // TODO Emojis should be wrapped in <span>, have role="img", and have an accessible description
   // with aria-label or aria-labelledby  jsx-a11y/accessible-emoji
-  return `/* eslint-disable jsx-a11y/accessible-emoji, no-unused-vars */
+  return `/* eslint-disable jsx-a11y/accessible-emoji, no-unused-vars, no-dupe-keys */
 ${getDependencies(state, getImport)}
 
 ${getStyles(state, name)}
