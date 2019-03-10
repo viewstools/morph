d7d7c29ca77228ca8ad4511442859bc677f70c9a
Darío Javier Cravero <dario@uxtemple.com>
2019-03-10 15:54:17 +0000

    chore: ugprade jest

--- rollup.config.js
+++ rollup.config.js
@@ -7,29 +7,12 @@ export default {
   output: {
     file: 'lib.js',
     format: 'cjs',
-    sourcemap: true,
+    sourcemap: false,
   },
   plugins: [
     babel({
-      plugins: ['external-helpers', 'transform-object-rest-spread'],
-      presets: [
-        [
-          'env',
-          {
-            targets: {
-              // React parses on ie 9, so we should too
-              ie: 11,
-              // We currently minify with uglify
-              // Remove after https://github.com/mishoo/UglifyJS2/issues/448
-              uglify: true,
-            },
-            // Disable polyfill transforms
-            useBuiltIns: false,
-            // Do not transform modules to CJS
-            modules: false,
-          },
-        ],
-      ],
+      babelrc: false,
+      presets: [['@babel/preset-env', { modules: false, useBuiltIns: false }]],
     }),
   ],
 }
