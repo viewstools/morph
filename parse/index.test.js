bf0738214cee04c4f1017b80d1398d3319a44c3b
Darío Javier Cravero <dario@uxtemple.com>
2019-04-01 17:09:15 +0100

    chore: update test fixtures

--- parse/index.test.js
+++ parse/index.test.js
@@ -8,70 +8,55 @@ test('#parse', () => {
 
 const VIEWS = [
   `Main Vertical`,
-
   `Main Vertical
-Text`,
-
+  Text`,
   `BlueButton Vertical
-Before Text
-
-Nested Vertical
-ImageInside Image
-source https://image.com/file.jpg
-
-VerticalInside Vertical
-
-
-After Text
-text after
-color purple
-when <isHovered
-color red
-
-Last Vertical
-backgroundColor blue`,
-
-  // ensure that empty spaces at the end of the block don't block the block from
-  // being recognised as a stop point for the props of the previous block
+  Before Text
+  Nested Vertical
+    ImageInside Image
+      source https://image.com/file.jpg
+    VerticalInside Vertical
+  After Text
+    text after
+    color purple
+    when <isHovered
+    color red
+  Last Vertical
+    backgroundColor blue`,
   `A Vertical
-A1 Text
-color red
-
-A2 Text
-color white`,
-
+  A1 Text
+    color red
+  A2 Text
+    color white`,
   `Vertical
-backgroundColor <backgroundColor
-onClick <onClick
-Title Text
-color < red
-marginLeft < 10
-text <title This is the title
-
-Capture
-type text
-onFocus <
-
-List
-from <
-Stuff`,
-
+  backgroundColor <backgroundColor
+  onClick <onClick
+  Title Text
+    color < red
+    marginLeft < 10
+    text <title This is the title
+  Capture
+    type text
+    onFocus <
+  List
+    from <
+    Stuff`,
   `Locals Vertical
-Text
-text hi
-when <es
-text hola`,
+  Text
+    text hi
+    when <es
+    text hola`,
 ]
 
 const WARNING = `Warning Vertical
-EmptyWhen Vertical
-color blue
-height 100
-marginTop <
-border 1px solid red
-height 302
-when
-color red
-when < stuff
-backgroundColor purple
-color green`
+  EmptyWhen Vertical
+    color blue
+    height 100
+    marginTop <
+    border 1px solid red
+    height 302
+    when
+    color red
+    when < stuff
+    backgroundColor purple
+    color green`
