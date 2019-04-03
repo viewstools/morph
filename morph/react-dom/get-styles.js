9a53966fd98572bc5f6310fe6bfefdb8467587bd
Darío Javier Cravero <dario@uxtemple.com>
2019-04-03 18:10:17 +0100

    fix: regression on styles being outputted as strings instead of objects

--- morph/react-dom/get-styles.js
+++ morph/react-dom/get-styles.js
@@ -1,4 +1,15 @@
 import { hasKeys } from '../utils.js'
 
-export default ({ styles }) =>
-  hasKeys(styles) ? `let styles = ${JSON.stringify(styles)}` : ''
+export default ({ styles }) => {
+  if (!hasKeys(styles)) return ''
+
+  let res = [`let styles = {`]
+
+  Object.entries(styles).forEach(([id, css]) => {
+    res.push(`${id}: ${css},`)
+  })
+
+  res.push('}')
+
+  return res.join('\n')
+}
