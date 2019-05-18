6e4e147af8305e9548806f84157ffe02cf848cf9
Alberto Escala <albertoaesc@gmail.com>
2019-05-18 09:53:40 -0500

    feat: add autogenerated comment on each .view.js file

--- morph/react/to-component.js
+++ morph/react/to-component.js
@@ -8,7 +8,8 @@ import getLocals from './get-locals.js'
 export default ({ getImport, getStyles, name, state }) => {
   // TODO Emojis should be wrapped in <span>, have role="img", and have an accessible description
   // with aria-label or aria-labelledby  jsx-a11y/accessible-emoji
-  return `/* eslint-disable jsx-a11y/accessible-emoji, no-unused-vars, no-dupe-keys */
+  return `// This file is auto-generated, please don't edit it. More info: https://github.com/viewstools/docs/blob/master/UseViews/README.md#viewjs-is-auto-generated-and-shouldnt-be-edited
+/* eslint-disable jsx-a11y/accessible-emoji, no-unused-vars, no-dupe-keys */
 ${getDependencies(state, getImport)}
 
 ${getStyles(state, name)}
