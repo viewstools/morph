f37e296c622cd0be1a8c6d6048db710fe175506f
Darío Javier Cravero <dario@uxtemple.com>
2019-05-28 21:12:22 +0100

    feat: delete empty folders when running --clean

--- clean.js
+++ clean.js
@@ -1,5 +1,6 @@
-import glob from 'fast-glob'
 import { promises as fs } from 'fs'
+import deleteEmpty from 'delete-empty'
+import glob from 'fast-glob'
 import path from 'path'
 
 export default async function clean(src) {
@@ -9,10 +10,13 @@ export default async function clean(src) {
     ignore: ['*node_modules*'],
   })
 
-  return Promise.all(
+  await Promise.all(
     morphed.map(f => {
       console.log(`x ${f}`)
       return fs.unlink(path.join(src, f))
     })
   )
+
+  let deleted = await deleteEmpty(src)
+  deleted.forEach(d => console.log(`x ${d}`))
 }
