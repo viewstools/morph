4e1ab3d585e3a4cc90608d7c27bdbb990a7a4591
Darío Javier Cravero <dario@uxtemple.com>
2019-06-06 22:18:32 +0100

    fix: only turn component into class if a ref in the component isn't a basic block

--- morph/react/property-ref.js
+++ morph/react/property-ref.js
@@ -2,7 +2,9 @@ import wrap from '../react/wrap.js'
 
 export function enter(node, parent, state) {
   if (node.name === 'ref') {
-    state.hasRefs = true
+    if (!parent.isBasic) {
+      state.hasRefs = true
+    }
 
     state.render.push(
       ` ${parent.isDynamic ? 'innerRef' : 'ref'}=${wrap(node.value)}`
