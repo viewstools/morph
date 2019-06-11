c4d45c0fd94098a6a6533287dedf27f0509cf3c3
Darío Javier Cravero <dario@uxtemple.com>
2019-06-11 15:53:25 +0100

    fix: morph isMedia on views too

--- __tests__/app/useIsMedia.js
+++ __tests__/app/useIsMedia.js
@@ -1,8 +1,8 @@
 import { useMedia as _useMedia } from 'use-media'
 
 let useIsMedia = () => {
-  let mobile = _useMedia({ minWidth: 0, maxWidth: 305 })
-  let tablet = _useMedia({ minWidth: 306, maxWidth: 1024 })
+  let mobile = _useMedia({ minWidth: 0, maxWidth: 375 })
+  let tablet = _useMedia({ minWidth: 376, maxWidth: 1024 })
   let laptop = _useMedia({ minWidth: 1025, maxWidth: 1280 })
   let desktop = _useMedia({ minWidth: 1281 })
 
