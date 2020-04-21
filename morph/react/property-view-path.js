4a50c73388e7bed784df697de936f3386f00d25c
Darío Javier Cravero <dario@uxtemple.com>
2020-04-21 11:22:57 +0200

    fix: actually, back to viewPath and added the actual view name to match how we use it in the view

--- morph/react/property-view-path.js
+++ morph/react/property-view-path.js
@@ -3,5 +3,5 @@ export function enter(node, parent, state) {
 
   // this is something we may use when going into dynamic paths through data
   // state.render.push(`viewPath={\`\${props.viewPath}/${state.name}\`}`)
-  state.render.push(`viewPath="${state.pathToStory}"`)
+  state.render.push(`viewPath="${state.pathToStory}/${node.name}"`)
 }
