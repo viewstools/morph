899da8cec89af2936c57ea1786a899b53632854f
Darío Javier Cravero <dario@uxtemple.com>
2020-04-21 11:21:03 +0200

    Revert "fix: rename viewPath to pathToView to indicate better what it really means"
    This reverts commit 3e53591493304bd36ed8c085159851496f0e6fc3.

--- morph/react-dom/block-properties.js
+++ morph/react-dom/block-properties.js
@@ -7,7 +7,7 @@ import * as PropertyRef from '../react/property-ref.js'
 import * as PropertyRest from '../react/property-rest.js'
 import * as PropertyStyle from '../react/property-style.js'
 import * as PropertyText from '../react/property-text.js'
-import * as PropertyPathToView from '../react/property-path-to-view.js'
+import * as PropertyViewPath from '../react/property-view-path.js'
 import { isColumn } from '../utils.js'
 import isValidPropertyForBlock from './is-valid-property-for-block.js'
 
@@ -18,7 +18,7 @@ export function enter(node, parent, state) {
   PropertiesClassName.enter(node, parent, state)
   PropertiesImage.enter(node, parent, state)
   PropertyFormat.enter(node, parent, state)
-  PropertyPathToView.enter(node, parent, state)
+  PropertyViewPath.enter(node, parent, state)
 
   node.properties.forEach(propNode => {
     if (
--- morph/react/property-view-path.js
+++ morph/react/property-view-path.js
@@ -2,6 +2,6 @@ export function enter(node, parent, state) {
   if (node.isBasic || !state.pathToStory) return
 
   // this is something we may use when going into dynamic paths through data
-  // state.render.push(`pathToView={\`\${props.viewPath}/${state.name}\`}`)
-  state.render.push(`pathToView="${state.pathToStory}"`)
+  // state.render.push(`viewPath={\`\${props.viewPath}/${state.name}\`}`)
+  state.render.push(`viewPath="${state.pathToStory}"`)
 }
