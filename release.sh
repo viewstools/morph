c24dd982f32e018454450e4b0e27abb2769d1a0b
Darío Javier Cravero <dario@uxtemple.com>
2021-03-11 14:03:05 +0100

    chore: $VERSION

--- release.sh
+++ release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
-VERSION=`npm version ${1:-patch}`
-git commit -am 'chore: $VERSION'
+VERSION=`npm version $1`
+git commit -am "chore: $VERSION"
 git tag $VERSION
 git push
 git push --tags
